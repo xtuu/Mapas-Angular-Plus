cb0b6b063c1611fc5bcad293abe63da8734b7ed6
Adrian Fernandez <adrianxtuu@gmail.com>
2023-03-26 17:49:15 -0400

    [UPDATE] macadores y popup

--- src/app/maps/components/map-view/map-view.component.ts
+++ src/app/maps/components/map-view/map-view.component.ts
@@ -1,5 +1,5 @@
 import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
-import { Map } from 'mapbox-gl';
+import { Map, Popup, Marker } from 'mapbox-gl';
 import { PlacesServices } from '../../services';
 
 @Component({
@@ -26,6 +26,17 @@ export class MapViewComponent implements AfterViewInit {
       center: this.placesServices.userLocation, // starting position [lng, lat]
       zoom: 14,
     });
+
+    const popup = new Popup()
+      .setHTML(`
+      <h6>Aqui estoy</h6>
+      <span>Estoy en este lugar del mundo</span>
+    `);
+
+    new Marker({ color: 'red' })
+      .setLngLat(this.placesServices.userLocation)
+      .setPopup(popup)
+      .addTo(map)
 
 
   }
