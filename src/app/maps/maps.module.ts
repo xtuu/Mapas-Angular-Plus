02ccbf15783eb52db09732578c967e5708709210
Adrian Fernandez <adrianxtuu@gmail.com>
2023-03-25 20:27:27 -0400

    [UPDATE] configuraciones iniciales

---
+++ src/app/maps/maps.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MapPageComponent } from './pages/map-page/map-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    MapPageComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    MapPageComponent
+  ]
+  
+})
+export class MapsModule { }
